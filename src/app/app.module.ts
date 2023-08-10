f0b399f62ab3655d01b111605db2c9cf68f857fc
chikipack <victorgotzilla@hotmail.com>
2023-08-09 21:22:46 -0700

    changes to the modules imported

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,9 +6,15 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HomeComponent } from './home/home.component';
 import { MatButtonModule } from '@angular/material/button';
-import { MyPokemonComponent } from './pokemon/my_pokemon/my-pokemon.component';
+import { MyPokemonComponent } from './pokemon/my-pokemon/my-pokemon.component';
+import { FindPokemonComponent } from './pokemon/find-pokemon/find-pokemon.component';
 @NgModule({
-  declarations: [AppComponent, HomeComponent, MyPokemonComponent],
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    MyPokemonComponent,
+    FindPokemonComponent,
+  ],
   imports: [
     BrowserModule,
     AppRoutingModule,
