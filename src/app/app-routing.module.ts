e208bda35baab14e994b059a2bedb465539f44b2
chikipack <victorgotzilla@hotmail.com>
2023-08-09 21:21:21 -0700

    routes to the new components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MyPokemonComponent } from './pokemon/my-pokemon/my-pokemon.component';
+import { HomeComponent } from './home/home.component';
+import { FindPokemonComponent } from './pokemon/find-pokemon/find-pokemon.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'my-pokemon', component: MyPokemonComponent },
+  { path: 'find-pokemon', component: FindPokemonComponent },
+  // Other routes go here if needed
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
