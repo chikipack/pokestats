e0767e612f8f89ffc874825ebb0be062b2e7dc24
chikipack <victorgotzilla@hotmail.com>
2023-08-09 21:04:05 -0700

    my pokemon component created

---
+++ src/app/pokemon/my_pokemon/my-pokemon/my-pokemon.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-my-pokemon',
+  templateUrl: './my-pokemon.component.html',
+  styleUrls: ['./my-pokemon.component.css']
+})
+export class MyPokemonComponent {
+
+}
